552ef894348f74bb87ccbf23d6f3b13d2785b1fa
vgherard <vgherard@sissa.it>
2021-07-14 19:33:41 +0200

    Add assert_is_string() helper

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,18 @@
+assert_is_string <- function(x, can_be_null)
+{
+	p <- can_be_null && is.null(x)
+	q <- is.character(x) && length(x) == 1 && !is.na(x)
+	if (p || q) return()
+
+	what <- paste0("'", deparse(substitute(x)), "'")
+	msg <- paste0(
+		what, " must be", ifelse(can_be_null, " either NULL or ", " "),
+		"a length one character (not NA)."
+		)
+
+	stop(msg)
+}
+
 ask_yesno_qn <- function(qn) {
 	prompt <- paste(qn, "(y/n)> ")
 	while (!is_valid_ans(ans <- readline(prompt)))
